e9830773f95689c32236815a201d03fd8c44893d
guangweima <45659706+guangweima@users.noreply.github.com>
2020-05-13 14:29:19 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -3,6 +3,7 @@ package com.usian.java;
 public class Test1 {
     //修改Test1
     public static void main(String[] args) {
-
+        //github端(其他同事)的修改
+        System.out.println("师姐你好");
     }
 }
